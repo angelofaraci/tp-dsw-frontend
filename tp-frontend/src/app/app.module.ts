81a87d53d397efaf1597fc629fbc40710a4bde5c
angelofaraci <angelofaraci089@yahoo.com>
2023-08-30 18:59:50 -0300

    Creacion de componentes, dependencias de bootstrap y poco mas

--- tp-frontend/src/app/app.module.ts
+++ tp-frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReviewsComponent,
+    HomeComponent,
+    SignInComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
