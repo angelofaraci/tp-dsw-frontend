683013a8d6b5c476f5b4b7504d2269d017c586e4
angelofaraci <angelofaraci089@yahoo.com>
2023-09-03 16:16:49 -0300

    Diseno del login

--- tp-frontend/src/app/app-routing.module.ts
+++ tp-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LogInComponent } from './log-in/log-in.component';
 //import { LogInComponent } from './log-in/log-in.component.js';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'login', component: LogInComponent},
   { path: 'home', component: HomeComponent },
   { path: '**',  redirectTo: '/home', pathMatch: 'full'}
 ];
