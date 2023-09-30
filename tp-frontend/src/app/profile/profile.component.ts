ecaa2747ad7b2acc9ce4f7665b8609d49c516eca
Nahuel <nahuelssjl@gmail.com>
2023-09-29 21:50:09 -0300

    Actualizacion de perfil e inicio de pagina de juego

--- tp-frontend/src/app/profile/profile.component.ts
+++ tp-frontend/src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -9,33 +10,45 @@ import { UserService } from '../services/user.service';
 export class ProfileComponent implements OnInit {
 
   user = {
-    username: '',
-    email: '',
-    profilePicture: ''
-
+    username: 'Username',
+    email: 'Email'
   }
 
-  constructor(private userService: UserService) {}
+  game ={
+    score: 30,
+  }
+  
+  constructor(private userService: UserService) {
+  }
+
 
   ngOnInit(){
     this.userService.getUserData()
       .subscribe(
         res =>{
           this.user = res.userData
-          console.log(this.user)
-          this.user.profilePicture = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
+          console.log(this.user)         
         },
         err => {
           console.log(err);
           
         }
       )
-  }
+      let color1=String(0)
+      let color2=String(0)
+      if(this.game.score >= 50){
+        color1=String((100-this.game.score)*2*2.55)
+        color2=String(255)
+      }
+      else{
+        color1=String(255)
+        color2=String(this.game.score*2*2.55)
+      }
+      document.documentElement.style.setProperty('--color1', color1);
+      document.documentElement.style.setProperty('--color2', color2);
+        
 
-  changeProfilePicture(image: string){
-    this.user.profilePicture = image
   }
-
   
 
 
