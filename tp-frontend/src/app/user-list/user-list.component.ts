598e64ab002416b3e068d0cfc3b3d63bd4309b86
angelofaraci <angelofaraci089@yahoo.com>
2024-04-10 21:41:34 -0300

    decoracion

--- tp-frontend/src/app/user-list/user-list.component.ts
+++ tp-frontend/src/app/user-list/user-list.component.ts
@@ -6,7 +6,7 @@ import { User } from '../interfaces/user.interface.js';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css'],
+  styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent implements OnInit {
   users: User[] = [];
