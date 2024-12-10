287138b332ef55b550020d7384bdc81a9bdb9846
angelofaraci <angelofaraci089@yahoo.com>
2024-12-10 20:49:14 -0300

    algo mas

--- tp-frontend/src/enviroments/enviroment.prod.ts
+++ tp-frontend/src/enviroments/enviroment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: 'mongodb+srv://angelofaraci:angelofaraci@cluster0.xlzr0.mongodb.net/tp-database?retryWrites=true&w=majority&appName=Cluster0'
-  };
+  production: true,
+  apiUrl: 'https://tp-dsw-backend.vercel.app/'
+};
