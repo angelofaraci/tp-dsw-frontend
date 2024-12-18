4afb8d1bc00b96186007d9777c38b9b4d8656814
unknown <nahuelverdera1@hotmail.com>
2024-12-18 11:57:24 -0300

    Agregado de nuevo el footer

--- tp-frontend/src/app/carousel/carousel.component.ts
+++ tp-frontend/src/app/carousel/carousel.component.ts
@@ -23,23 +23,7 @@ export class CarouselComponent implements OnInit {
     rating: 0,
   };
   
-  featuredReviews: any = [
-    {
-      title: 'Undertale',
-      img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.deviantart.com%2Fignysound%2Fart%2FUndertale-Banner-563650549&psig=AOvVaw3GPXas3YtD2bnv-iTnyPh9&ust=1723921909404000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLjl_9ib-ocDFQAAAAAdAAAAABAJ',
-      rating: 75
-    },
-    {
-     title: 'Super Mario Odyssey',
-     img: 'https://i.imgur.com/9NlsvBJ.png',
-     rating: 65,
-    },
-    {
-     title: 'Sonic Superstars',
-     img: 'https://sonicsuperstars.com/img/hero/herobg_1920.jpg',
-     rating: 35,
-    },
-   ];
+  featuredReviews: any = [];
 
    calculateColorRating(number: number){
     if (number>=70) {
